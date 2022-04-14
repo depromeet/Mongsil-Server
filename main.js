750b2ed2e5f0862b1a78f7803a9c93cba8818c34
homelala <homelala@naver.com>
2022-04-15 00:09:14 +0900

    add: setting Sequelize

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const { sequelize } = require('./models');
 const app = express();
+app.set('port', process.env.PORT || 3000);
 
-app.get('/test', (req,res) => {
-    res.send('success test!')
-})
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결됨.');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-app.listen(80, function () {
-    console.log('Example app listening on port 80!');
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public'))); // 요청시 기본 경로 설정
+app.use(express.json()); // json 파싱
+app.use(express.urlencoded({ extended: false })); // uri 파싱
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+// 에러 처리 미들웨어
+app.use((err, req, res, next) => {
+  // 템플릿 변수 설정
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; // 배포용이 아니라면 err설정 아니면 빈 객체
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
