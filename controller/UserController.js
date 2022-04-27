f4b72a10ecafc7fe0fe256d4409ba51a50b9ff0a
homelala <homelala@naver.com>
2022-04-27 21:39:16 +0900

    add: 회원 가입 테스트

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,16 +1,25 @@
-const UserService = require('../models/service/UserService');
+const UserService = require('../models/service/user/UserService');
+const validation = require('../libs/validations');
+const UserServiceError = require('../models/Error');
 module.exports = {
-  findAllUser: async function(req, res){
-    res.send(await UserService.allUser()); 
+  findAllUser: async function (req, res) {
+    res.send(await UserService.allUser());
   },
-  saveUser: async function(req, res){
-    try{
-      let userId = await UserService.saveUser(req.body)
-      res.status(200).json({"userId":userId});
-    }catch(err){
-      console.log("ff")
+  saveUser: async function (req, res) {
+    try {
+      const validationValue = validation.userSaveValidation(req.body);
+      if (validationValue) {
+        throw new Error(validationValue);
+      } else {
+        let userId = await UserService.saveUser(
+          req.body.userName,
+          req.body.userEmail
+        );
+        res.status(200).json({ userId: userId });
+      }
+    } catch (err) {
       console.log(err);
-      res.status(500).send(err);
+      res.status(500).send(new UserServiceError(err.message));
     }
-  }
-}
+  },
+};
--- models/repository/UserRepository.js
+++ models/repository/UserRepository.js
@@ -1,21 +1,27 @@
-const Sequelize = require('../index')
+const Sequelize = require('../index');
 module.exports = {
-  findAll: async function(){
-    return await Sequelize.User.findAll()
+  findAll: async function () {
+    return await Sequelize.User.findAll();
   },
-  save: async function(userName, userEmail, transaction){
-      const user = await Sequelize.User.create({
+  save: async function (userName, userEmail, transaction) {
+    const user = await Sequelize.User.create(
+      {
         name: userName,
-        email: userEmail
-      }, {transaction:transaction});
-      return user;
-  },
-  findByEmail: async function(userEmail, transaction){
-    const user =  await Sequelize.User.findOne({
-      where: {
-        email: userEmail
-      }
-    }, {transaction:transaction});
+        email: userEmail,
+      },
+      { transaction: transaction }
+    );
     return user;
   },
-}
+  findByEmail: async function (userEmail, transaction) {
+    const user = await Sequelize.User.findOne(
+      {
+        where: {
+          email: userEmail,
+        },
+      },
+      { transaction: transaction }
+    );
+    return user;
+  },
+};
