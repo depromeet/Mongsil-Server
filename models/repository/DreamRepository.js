d98e48b24e123d2801a025d880b329936905ff80
lsh <rrgks@naver.com>
2022-05-13 19:58:01 +0900

    Delete: 분리하기 전 메서드 제거

--- models/repository/DreamRepository.js
+++ models/repository/DreamRepository.js
@@ -43,35 +43,6 @@ module.exports = {
     }
   },
 
-  findAllCategory: async () => {
-    try {
-      const noun = await Sequelize.BigCategory.findAll({
-        attributes: ['name'],
-        where: {
-          name: {
-            [Op.ne]: '동사/형용사',
-          },
-        },
-        include: [
-          {
-            model: Sequelize.Category,
-            attributes: ['name'],
-          },
-        ],
-      });
-      const verbAndAdjective = await Sequelize.Category.findAll({
-        attributes: ['name'],
-        where: {
-          bigCategoryId: 4,
-        },
-      });
-
-      return { noun, verbAndAdjective };
-    } catch (err) {
-      throw err;
-    }
-  },
-
   findOneCategoryByKeword: async (keyword) => {
     try {
       const kewordId = await Sequelize.Category.findAll({
