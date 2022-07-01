4a3ec15fc4683efd2dde556ae676e8cb56790556
homelala <homelala@naver.com>
2022-07-01 12:01:43 +0900

    fix: dreamCard registerDate

--- controller/DreamCardController.js
+++ controller/DreamCardController.js
@@ -13,7 +13,7 @@ module.exports = {
 
       res
         .status(200)
-        .send(new ResponseDto(200, '저장 완료', new DreamCardDto(cardInfo)));
+        .send(new ResponseDto(200, 'success', new DreamCardDto(cardInfo)));
     } catch (err) {
       console.log(err);
       res.status(403).send(new DreamCardServiceError(err.message));
@@ -24,14 +24,16 @@ module.exports = {
     try {
       transaction = await sequelize.transaction();
 
-      const { userId, title, description } = req.body;
+      const { userId, title, description, registerDate } = req.body;
       const categories =
         req.body.categories.length == 0 ? [232] : req.body.categories;
       const cardId = await dreamCardService.save(
         userId,
         title,
         description,
-        categories
+        categories,
+        registerDate,
+        transaction
       );
       await transaction.commit();
 
